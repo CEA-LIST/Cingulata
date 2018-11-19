12ce35a1548adb2aa05d80035c83e44bddb0c786
Sergiu Carpov <sergiu.carpov@cea.fr>
2018-11-19 22:48:46 +0100

    CiBit unittest for plaintext

--- common/test/unittest/test_ci_bit.cxx
+++ common/test/unittest/test_ci_bit.cxx
@@ -6,37 +6,173 @@
 using namespace std;
 using namespace cingulata;
 
-TEST(CiBit_PlainText, Op2Inp) {
-  const int tt[4][10] =
-  {
-  //   and nand    andny andyn or    nor   orny  oryn  xor   xnor   bool function
-                                                                    /* x y */
-    {  0,    1,    0,    0,    0,    1,    1,    1,    0,    1   }, /* 0 0 */
-    {  0,    1,    1,    0,    1,    0,    1,    0,    1,    0   }, /* 0 1 */
-    {  0,    1,    0,    1,    1,    0,    0,    1,    1,    0   }, /* 1 0 */
-    {  1,    0,    0,    0,    1,    0,    1,    1,    0,    1   }  /* 1 1 */
-  };
+tuple<
+  string,
+  std::function<void (CiBit&, const CiBit&)>,
+  vector<int>
+> obj_operators[] =
+{
+  { "op_not",   [](CiBit& a, const CiBit& b) { a.op_not();    },  {1,1,0,0} },
+  { "op_and",   [](CiBit& a, const CiBit& b) { a.op_and(b);   },  {0,0,0,1} },
+  { "op_nand",  [](CiBit& a, const CiBit& b) { a.op_nand(b);  },  {1,1,1,0} },
+  { "op_andny", [](CiBit& a, const CiBit& b) { a.op_andny(b); },  {0,1,0,0} },
+  { "op_andyn", [](CiBit& a, const CiBit& b) { a.op_andyn(b); },  {0,0,1,0} },
+  { "op_or",    [](CiBit& a, const CiBit& b) { a.op_or(b);    },  {0,1,1,1} },
+  { "op_nor",   [](CiBit& a, const CiBit& b) { a.op_nor(b);   },  {1,0,0,0} },
+  { "op_orny",  [](CiBit& a, const CiBit& b) { a.op_orny(b);  },  {1,1,0,1} },
+  { "op_oryn",  [](CiBit& a, const CiBit& b) { a.op_oryn(b);  },  {1,0,1,1} },
+  { "op_xor",   [](CiBit& a, const CiBit& b) { a.op_xor(b);   },  {0,1,1,0} },
+  { "op_xnor",  [](CiBit& a, const CiBit& b) { a.op_xnor(b);  },  {1,0,0,1} },
 
-  pair<string,std::function<void (CiBit&, const CiBit&)>> operators[] {
-    { "op_and",   [](CiBit& a, const CiBit& b) { a.op_and(b);   } },
-    { "op_nand",  [](CiBit& a, const CiBit& b) { a.op_nand(b);  } },
-    { "op_andny", [](CiBit& a, const CiBit& b) { a.op_andny(b); } },
-    { "op_andyn", [](CiBit& a, const CiBit& b) { a.op_andyn(b); } },
-    { "op_or",    [](CiBit& a, const CiBit& b) { a.op_or(b);    } },
-    { "op_nor",   [](CiBit& a, const CiBit& b) { a.op_nor(b);   } },
-    { "op_orny",  [](CiBit& a, const CiBit& b) { a.op_orny(b);  } },
-    { "op_oryn",  [](CiBit& a, const CiBit& b) { a.op_oryn(b);  } },
-    { "op_xor",   [](CiBit& a, const CiBit& b) { a.op_xor(b);   } },
-    { "op_xnor",  [](CiBit& a, const CiBit& b) { a.op_xnor(b);  } }
-  };
+  { "oper +=",  [](CiBit& a, const CiBit& b) { a += b; },         {0,1,1,0} },
+  { "oper -=",  [](CiBit& a, const CiBit& b) { a -= b; },         {0,1,1,0} },
+  { "oper *=",  [](CiBit& a, const CiBit& b) { a *= b; },         {0,0,0,1} },
+  { "oper &=",  [](CiBit& a, const CiBit& b) { a &= b; },         {0,0,0,1} },
+  { "oper |=",  [](CiBit& a, const CiBit& b) { a |= b; },         {0,1,1,1} },
+  { "oper ^=",  [](CiBit& a, const CiBit& b) { a ^= b; },         {0,1,1,0} },
+                                                             // b  0 1 0 1
+                                                             // a  0 0 1 1
+  { "", nullptr, {}}
+};
 
-  for (int idx = 0; idx < 10; ++idx)
+TEST(CiBit_PT, oper_2_cibit_input) {
+
+  for (int idx = 0; ; ++idx)
   {
-    for (int v = 0; v < 4; ++v) {
-      CiBit a(1 & (v >> 1));
-      CiBit b(1 & v);
-      operators[idx].second(a,b);
-      ASSERT_EQ(tt[v][idx], a.get_val()) << " operator " << operators[idx].first;
+    auto& op_name = get<0>(obj_operators[idx]);
+    auto& op_func = get<1>(obj_operators[idx]);
+    auto& op_tt = get<2>(obj_operators[idx]);
+
+    if (op_name.empty()) break;
+
+    for (int v = 0; v < op_tt.size(); ++v) {
+      int b1 = 1 & (v >> 1);
+      CiBit a(b1);
+      int b0 = 1 & (v >> 0);
+      CiBit b(b0);
+
+      op_func(a, b);
+
+      // operator result is good
+      ASSERT_EQ(op_tt[v], (int)a.get_val())
+        << " operator '" << op_name << "'" << " (" << b1 << "," << b0 << ")";
+
+      // value of b does not change
+      ASSERT_EQ(b0, (int)b.get_val()) << " operator '" << op_name << "'";
+
+      // stays plain afterwards
+      ASSERT_TRUE(a.is_plain()) << " operator '" << op_name << "'";
+      ASSERT_TRUE(b.is_plain()) << " operator '" << op_name << "'";
     }
   }
 }
+
+TEST(CiBit_PT, oper_1_cibit_1_pt_input) {
+
+  for (int idx = 0; ; ++idx)
+  {
+    auto& op_name = get<0>(obj_operators[idx]);
+    auto& op_func = get<1>(obj_operators[idx]);
+    auto& op_tt = get<2>(obj_operators[idx]);
+
+    if (op_name.empty()) break;
+
+    for (int v = 0; v < op_tt.size(); ++v) {
+      int b1 = 1 & (v >> 1);
+      CiBit a(b1);
+
+      int b0 = 1 & (v >> 0);
+
+      op_func(a,b0);
+
+      ASSERT_EQ(op_tt[v], (int)a.get_val())
+        << " operator '" << op_name << "'" << " (" << b1 << "," << b0 << ")";
+
+      // stays plain afterwards
+      ASSERT_TRUE(a.is_plain()) << " operator '" << op_name << "'";
+    }
+  }
+}
+
+tuple<
+  string,
+  std::function<CiBit (const CiBit&, const CiBit&)>,
+  vector<int>
+> operators[] =
+{
+  { "oper + ",  [](const CiBit& a, const CiBit& b) { return a + b;  },  {0,1,1,0} },
+  { "oper - ",  [](const CiBit& a, const CiBit& b) { return a - b;  },  {0,1,1,0} },
+  { "oper * ",  [](const CiBit& a, const CiBit& b) { return a * b;  },  {0,0,0,1} },
+  { "oper ~ ",  [](const CiBit& a, const CiBit& b) { return    ~a;  },  {1,1,0,0} },
+  { "oper ^ ",  [](const CiBit& a, const CiBit& b) { return a ^ b;  },  {0,1,1,0} },
+  { "oper & ",  [](const CiBit& a, const CiBit& b) { return a & b;  },  {0,0,0,1} },
+  { "oper | ",  [](const CiBit& a, const CiBit& b) { return a | b;  },  {0,1,1,1} },
+
+  { "oper ! ",  [](const CiBit& a, const CiBit& b) { return    !a;  },  {1,1,0,0} },
+  { "oper &&",  [](const CiBit& a, const CiBit& b) { return a && b; },  {0,0,0,1} },
+  { "oper ||",  [](const CiBit& a, const CiBit& b) { return a || b; },  {0,1,1,1} },
+
+  { "oper ==",  [](const CiBit& a, const CiBit& b) { return a == b; },  {1,0,0,1} },
+  { "oper !=",  [](const CiBit& a, const CiBit& b) { return a != b; },  {0,1,1,0} },
+  { "oper < ",  [](const CiBit& a, const CiBit& b) { return a < b;  },  {0,1,0,0} },
+  { "oper <= ", [](const CiBit& a, const CiBit& b) { return a <= b; },  {1,1,0,1} },
+  { "oper > ",  [](const CiBit& a, const CiBit& b) { return a > b;  },  {0,0,1,0} },
+  { "oper >=",  [](const CiBit& a, const CiBit& b) { return a >= b; },  {1,0,1,1} },
+                                                                   // b  0 1 0 1
+                                                                   // a  0 0 1 1
+
+  { "", nullptr, {}}
+};
+
+TEST(CiBit_PT, ext_oper_2_cibit_input) {
+  for (int idx = 0; ; ++idx)
+  {
+    auto& op_name = get<0>(operators[idx]);
+    auto& op_func = get<1>(operators[idx]);
+    auto& op_tt = get<2>(operators[idx]);
+
+    if (op_name.empty()) break;
+
+    for (int v = 0; v < op_tt.size(); ++v) {
+      int b1 = 1 & (v >> 1);
+      CiBit a(b1);
+      int b0 = 1 & (v >> 0);
+      CiBit b(b0);
+
+      CiBit c = op_func(a, b);
+
+      // operator result is good
+      ASSERT_EQ(op_tt[v], (int)c.get_val())
+        << " operator '" << op_name << "'" << " (" << b1 << "," << b0 << ")";
+
+      // value of b does not change
+      ASSERT_EQ(b1, (int)a.get_val()) << " operator '" << op_name << "'";
+      ASSERT_EQ(b0, (int)b.get_val()) << " operator '" << op_name << "'";
+
+      // stays plain afterwards
+      ASSERT_TRUE(a.is_plain()) << " operator '" << op_name << "'";
+      ASSERT_TRUE(b.is_plain()) << " operator '" << op_name << "'";
+      ASSERT_TRUE(c.is_plain()) << " operator '" << op_name << "'";
+    }
+  }
+}
+
+TEST(CiBit, set_name) {
+  CiBit a;
+  string test_name = "lol";
+
+  a.set_name(test_name);
+  ASSERT_EQ(test_name, a.get_name());
+
+  a.set_name(test_name + test_name);
+  ASSERT_EQ(test_name + test_name, a.get_name());
+}
+
+TEST(CiBit, clr_name) {
+  CiBit a;
+  string test_name = "lol";
+  a.set_name(test_name);
+  a.clr_name();
+  ASSERT_NE(test_name, a.get_name());
+}
+
