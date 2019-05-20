84f4346c08478ce63f7c9b1974c10b54f0211b26
RUF <patrick.ruf@cea.fr>
2019-05-20 08:06:51 +0200

    modif tests passage en V2 interface de sortie de 4 a 8 bits .cxx run.sh.in

---
+++ tests/wiretap/README.md
@@ -0,0 +1,9 @@
+wiretap
+=======
+
+Tell if a word appears in a list of words and, if so, returns the index at which it occurs for the first time, or otherwise -1 is returned.
+
+Words are represented as hash values.
+
+
+
---
+++ tests/wiretap/wiretap.cxx
@@ -0,0 +1,55 @@
+/*
+    (C) Copyright 2019 CEA LIST. All Rights Reserved.
+    Contributor(s): Cingulata team (formerly Armadillo team)
+
+    This software is governed by the CeCILL-C license under French law and
+    abiding by the rules of distribution of free software.  You can  use,
+    modify and/ or redistribute the software under the terms of the CeCILL-C
+    license as circulated by CEA, CNRS and INRIA at the following URL
+    "http://www.cecill.info".
+
+    As a counterpart to the access to the source code and  rights to copy,
+    modify and redistribute granted by the license, users are provided only
+    with a limited warranty  and the software's author,  the holder of the
+    economic rights,  and the successive licensors  have only  limited
+    liability.
+
+    The fact that you are presently reading this means that you have had
+    knowledge of the CeCILL-C license and that you accept its terms.
+*/
+
+#include <cstdint>
+
+/* local includes */
+#include <bit_exec/tracker.hxx>
+#include <ci_context.hxx>
+#include <ci_int.hxx>
+#include <int_op_gen/mult_depth.hxx>
+
+/* namespaces */
+using namespace std;
+using namespace cingulata;
+
+#define QUERY_SIZE 10
+
+int main()
+{
+	//Integer32 w,q[QUERY_SIZE];
+    CiInt w{CiInt::s32};
+    vector<CiInt> q(QUERY_SIZE,CiInt::u32);
+	cin>>w;
+	for(int i=0;i<QUERY_SIZE;i++)
+		cin>>q[i];
+	
+    CiInt r{0, 4, false};// result of 4 bits in run.sh.in
+	//Integer8 r=0;
+	for(int i=0;i<QUERY_SIZE;i++)
+		r+=(w==q[i])*CiInt(i+1,8,false); //4 ?
+	r-=1;
+	//r&=0xf; 
+
+	//cout<<r;
+	r.write("s");
+	CiContext::get_bit_exec_t<BitTracker>()->export_blif(blif_name, "wiretap");
+}
+
