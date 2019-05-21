f016513f4f3b958c2779cedb095436d04accc6b6
pa222727 <p.aubry@cea.fr>
2019-05-21 11:30:09 +0200

    Multi input adder revision -- pass unittest

--- common/src/int_op_gen/impl/multi_inp_adder.cxx
+++ common/src/int_op_gen/impl/multi_inp_adder.cxx
@@ -17,21 +17,20 @@ CiBitVector MultiInputAdder::oper(const std::vector<CiBitVector> &inps) const {
   vector<CiBitVector> inps_sym;
   for (const CiBitVector& inp: inps) {
     CiBitVector inp_sym(inp.size(), 1); // encode maximal inp.size()-bit value
-
     /* set plaintext bits if available */
     for (unsigned i = 0; i < inp.size(); ++i) {
-      if (inp[i].is_plain() and inp[i].get_val() == 0)
+      if (inp[i].is_plain() and inp[i].get_val() == 0){
         inp_sym[i] = 0;
+      }
     }
 
     inps_sym.push_back(inp_sym);
   }
 
   CiBitVector sum_sym = add(inps_sym);
-
   /* first set bit, starting from MSB, gives output size */
   int out_size = sum_sym.size();
-  while (sum_sym[out_size-1].get_val() == 0 and out_size > 0) out_size--;
+  while (sum_sym[out_size-1].get_val() == 0 and out_size > 1) out_size--;
 
   /* actual computation */
   return add(inps, out_size);
@@ -64,20 +63,15 @@ CiBitVector MultiInputAdder::add(const std::vector<CiBitVector> &inps, const uns
 
     int stmp = (sa > sb ? sa : sb);
     unsigned sr = (sc > stmp ? sc : stmp);
-    sr = sr > max_size ? max_size : sr;
-    CiBitVector tmp1 = a ^ b ^ c;
     /* equalize sizes */
     a.resize(sr);
     b.resize(sr);
     c.resize(sr);
-    /* increase size if necessary */
-    if (sr < max_size)
-      if ( a[sr-1].get_val()==1 or b[sr-1].get_val()==1 or c[sr-1].get_val()==1){
-        sr+=1;
-        a.resize(sr);
-        b.resize(sr);
-        c.resize(sr);
-      }
+    CiBitVector tmp1 = a ^ b ^ c;
+    sr = sr+1 > max_size ? max_size : sr+1;
+    a.resize(sr);
+    b.resize(sr);
+    c.resize(sr);
     a >>= 1;
     b >>= 1;
     c >>= 1;
@@ -97,16 +91,11 @@ CiBitVector MultiInputAdder::add(const std::vector<CiBitVector> &inps, const uns
   tie(db, b) = elems_sorted_by_depth.top();
   elems_sorted_by_depth.pop();
 
-  unsigned sr = (a.size() > b.size() ? a.size() : b.size());
+  unsigned sr = (a.size() > b.size() ? a.size() : b.size())+1;
+  sr = sr > max_size ? max_size : sr;
   /* equalize sizes */
   a.resize(sr);
   b.resize(sr);
-  if (sr < max_size)
-    if ( a[sr-1].get_val()==1 or b[sr-1].get_val()==1){
-      sr+=1;
-      a.resize(sr);
-      b.resize(sr);
-    }
   return bin_adder(a,b);
 }
 
